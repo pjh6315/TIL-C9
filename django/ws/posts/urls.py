d967c7d95dc9654920daa7856dd22d77f445116f
JaeHyoung Park <bluetruddnjs1@gmail.com>
2019-01-29 08:34:37 +0000

    D20 | 190129 PM | CRUD

--- django/ws/posts/urls.py
+++ django/ws/posts/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('create/',views.create),
     path('new/',views.new),
+    path('<int:post_id>/', views.detail),
+    path('<int:post_id>/delete/', views.delete),
+    path('<int:post_id>/edit/', views.edit),
+    path('<int:post_id>/update/', views.update),
 ]
--- django/ws/posts/views.py
+++ django/ws/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 # Create your views here.
 
@@ -6,12 +6,44 @@ def new(request):
     return render(request,'new.html')
     
 def create(request):
-    name = request.GET.get('name')
-    email = request.GET.get('email')
-    birthday = request.GET.get('birthday')
-    age = request.GET.get('age')
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    birthday = request.POST.get('birthday')
+    age = request.POST.get('age')
     # DB INSERT
     post = Post(name=name,email=email,birthday=birthday,age=age)
     post.save()
 
-    return render(request,'create.html')
+    return redirect(f'/posts/{post.pk}/')
+    
+def index(request):
+    # ALL POST
+    posts = Post.objects.all()
+    return render(request, 'index.html',{'posts':posts})
+    
+def detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    
+    return render(request,'detail.html',{'post':post})
+    
+def delete(request, post_id):
+    # 삭제하는 코드
+    post = Post.objects.get(pk=post_id)
+    post.delete()
+    
+    return redirect('/posts/')
+    
+def edit(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    
+    return render(request, 'edit.html', {'post':post})
+    
+def update(request, post_id):
+    # 수정하는코드
+    post = Post.objects.get(pk=post_id)
+    post.name = request.POST.get('name')
+    post.email = request.POST.get('email')
+    post.birthday = request.POST.get('birthday')
+    post.age = request.POST.get('age')
+    post.save()
+    return redirect(f'/posts/{post_id}/')
